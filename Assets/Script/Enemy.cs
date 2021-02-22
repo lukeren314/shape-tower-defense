aecbab29c32c342d1772b2c80f0674c1733623ca
lukeren314 <lukeren314@gmail.com>
2021-02-21 21:26:52 -0800

    added enemy scripts

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public PathData path;
+    public PathData pathData;
     public EnemyData enemyData;
     public float currentHealth;
 
+    private int currentEdge;
+    private float currentEdgeLength;
+    private float edgeDistance;
+    private float totalDistance;
 
+    public void DoUpdate()
+    {
+        // move to current edge distance
+        
+    }
+
+    public void FixedUpdate()
+    {
+        if (edgeDistance < currentEdgeLength)
+        {
+            edgeDistance += Time.deltaTime * enemyData.speed;
+        } else
+        {
+            NextEdge();
+        }
+    }
+    private void NextEdge()
+    {
+        ++currentEdge;
+        if (currentEdge < pathData.points.Count)
+        {
+            currentEdgeLength = Vector2.Distance(pathData.points[currentEdge], pathData.points[currentEdge + 1]);
+        } else
+        {
+            DealDamage();
+        }
+    }
+    private void DealDamage()
+    {
+        Destroy(this);
+    }
 }
